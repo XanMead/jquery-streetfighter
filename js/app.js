b0ea768389c07baa184a80ff06269e27d7e359f4
XanMead <x.m.mead@gmail.com>
2015-03-02 13:34:58 -0500

    fixed hadouken reset; added hadouken sound

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 	});
 
 	$('.ryu').mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show().animate(
@@ -17,7 +18,7 @@ $(document).ready(function() {
 			500,
 			function() {
 				$(this).hide();
-				$(this).css('left', '494px');
+				$(this).css('left', '625px');
 			}
 		);
 		// hadouken sound
@@ -28,4 +29,10 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+	function playHadouken() {
+		$('#hadouken-sound')[0].volume = 0.5
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
 });
