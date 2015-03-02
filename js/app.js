c597eea7bb37376bf38b88aa45b1008ff689d80c
XanMead <x.m.mead@gmail.com>
2015-03-02 17:14:12 -0500

    removed playHadouken function from document.ready() body; increased hadouken animation distance

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
-		$('.ryu-ready').show();
+		$('.ryu-re	ady').show();
 	});
 
 	$('.ryu').mouseleave(function() {
@@ -14,25 +14,23 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show().animate(
-			{'left': '1020px'},
+			{'left': '1200px'},
 			500,
 			function() {
 				$(this).hide();
 				$(this).css('left', '625px');
 			}
 		);
-		// hadouken sound
-		// animate hadouken
 	});
 
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+});
 
-	function playHadouken() {
+function playHadouken() {
 		$('#hadouken-sound')[0].volume = 0.5
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
-	}
-});
+}
