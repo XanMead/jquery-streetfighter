b082f49d063ce5629a93f576d0fc5ba2ba864e68
XanMead <x.m.mead@gmail.com>
2015-03-02 13:02:41 -0500

    Established jQuery mouse events

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	});
+
+	$('.ryu').mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	});
+
+	$('.ryu').mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// hadouken sound
+		// animate hadouken
+	});
+
+	$('.ryu').mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
